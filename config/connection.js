b6dbf26bb18a2901eb927666fd390ade5563ed76
Zkinsk <zoolunk@gmail.com>
2019-02-24 23:45:51 -0500

    config files

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  var connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
