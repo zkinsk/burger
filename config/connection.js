938886f1963d9190fc39bb1f0943850caeff9b3b
Scott Zinski <zoolunk@gmail.com>
2020-08-15 15:33:01 -0400

    Update Connection File

--- config/connection.js
+++ config/connection.js
@@ -6,9 +6,9 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   var connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
+    host: "127.0.0.1",
+    port: "3306",
+    user: "root",
     password: process.env.DB_PASS,
     database: "burgers_db"
   });
